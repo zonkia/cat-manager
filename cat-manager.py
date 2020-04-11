8615fef11b0ceb3298adeb1282130abf0da44ae1
zonkia <zonkia@gmail.com>
2020-04-11 17:42:39 +0200

    Improved spelling and linting

--- cat-manager.py
+++ cat-manager.py
@@ -5,58 +5,68 @@ from pprint import pprint
 from enum import IntEnum
 
 header = {
-        "x-api-key": "[YOUR API KEY]"
-        }
+    "x-api-key": "[YOUR_API_KEY]"
+}
 
 Menu = IntEnum("Menu", "zaloguj dodaj ulubione usun")
 usrId = ""
 
+
 def get_random_cat():
     parameters = {
-            "limit": 1
-            }
-    try:
-        allCatsJson = requests.get("https://api.thecatapi.com/v1/images/search", parameters, headers = header).json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format", allCatsJson.text)
-    return allCatsJson
-
-def add_to_favourites(catId, usrId):
-    usrCatParameters = {
-                "image_id": catId,
-                "sub_id": usrId
-                }
-    try:
-        addToFavourites = requests.post("https://api.thecatapi.com/v1/favourites", json = usrCatParameters, headers = header).json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format", addToFavourites.text)
-    print("Dodano do ulubionych")
-    return addToFavourites
-
-def view_favourites(usrId):
-    parameters = {
-                "limit": 100,
-                "sub_id": usrId
-                }
-    try:
-        FavouriteCatsJson = requests.get("https://api.thecatapi.com/v1/favourites", parameters, headers = header).json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format", FavouriteCatsJson.text)
-    return FavouriteCatsJson
-
-def delete_favourite(usrId, favId):
-    try:
-        deleteFavourite = requests.delete("https://api.thecatapi.com/v1/favourites/" + favId, headers = header).json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format", deleteFavourite.text)
-    return deleteFavourite
-
-def view_favourite_ids(view_favourites, usrId):
-    favouriteCats = view_favourites(usrId)
-    favouriteIds_dict = {cats["id"]: cats["image"]["url"]
-                        for cats in favouriteCats
+        "limit": 1
     }
-    return favouriteIds_dict
+    try:
+        randomCatJson = requests.get(
+            "https://api.thecatapi.com/v1/images/search", parameters, headers=header).json()
+    except json.decoder.JSONDecodeError:
+        print("Niepoprawny format", randomCatJson.text)
+    return randomCatJson[0]
+
+
+def add_to_favorites(catId, usrId):
+    usrCatParameters = {
+        "image_id": catId,
+        "sub_id": usrId
+    }
+    try:
+        addToFavorites = requests.post(
+            "https://api.thecatapi.com/v1/favorites", json=usrCatParameters, headers=header).json()
+    except json.decoder.JSONDecodeError:
+        print("Niepoprawny format", addToFavorites.text)
+    print("Dodano do ulubionych")
+    return addToFavorites
+
+
+def view_favorites(usrId):
+    parameters = {
+        "limit": 100,
+        "sub_id": usrId
+    }
+    try:
+        FavoriteCatsJson = requests.get(
+            "https://api.thecatapi.com/v1/favorites", parameters, headers=header).json()
+    except json.decoder.JSONDecodeError:
+        print("Niepoprawny format", FavoriteCatsJson.text)
+    return FavoriteCatsJson
+
+
+def delete_favorite(usrId, favId):
+    try:
+        deleteFavorite = requests.delete(
+            "https://api.thecatapi.com/v1/favorites/" + favId, headers=header).json()
+    except json.decoder.JSONDecodeError:
+        print("Niepoprawny format", deleteFavorite.text)
+    return deleteFavorite
+
+
+def view_favorite_ids(view_favorites, usrId):
+    favoriteCats = view_favorites(usrId)
+    favoriteIds_dict = {cats["id"]: cats["image"]["url"]
+                        for cats in favoriteCats
+                        }
+    return favoriteIds_dict
+
 
 while True:
     while True:
@@ -67,7 +77,7 @@ while True:
             if usrId != "":
                 True
             else:
-                print("1. Zaloguj się")   
+                print("1. Zaloguj się")
             print("2. Wylosuj kota i dodaj do ulubionych")
             print("3. Pokaż ulubione")
             print("4. Usuń kota z listy ulubionych")
@@ -89,41 +99,42 @@ while True:
         continue
 
     if menuChoice == Menu.dodaj:
-        #sprawdzamy czy użytkownik jest zalogowany
+        # sprawdzamy czy użytkownik jest zalogowany
         if usrId == "":
             print("Musisz się najpierw zalogować")
             usrId = input("Podaj swoje imię: ")
         # pobieramy losowego kota
-        allCatsJson = get_random_cat()
-        catId = allCatsJson[0]["id"]
-        catImgUrl = allCatsJson[0]["url"]
+        randomCatJson = get_random_cat()
+        catId = randomCatJson["id"]
+        catImgUrl = randomCatJson["url"]
         # otwieramy kota w nowej zakładce
-        webbrowser.open_new_tab(allCatsJson[0]["url"])
+        webbrowser.open_new_tab(randomCatJson["url"])
         print()
-        #zalogowany użytkownik dodaje kotka do ulubionych
-        addFavourite = input("Dodać do ulubionych? T/N: ").upper()
-        if addFavourite == "T":
-            add_to_favourites(catId, usrId)
+        # zalogowany użytkownik dodaje kotka do ulubionych
+        addFavorite = input("Dodać do ulubionych? T/N: ").upper()
+        if addFavorite == "T":
+            add_to_favorites(catId, usrId)
         else:
             print("Trudno")
         print()
         continue
 
-    #sprawdzamy ulubione
+    # sprawdzamy ulubione
     if menuChoice == Menu.ulubione:
-        #sprawdzamy czy użytkownik jest zalogowany
+        # sprawdzamy czy użytkownik jest zalogowany
         if usrId == "":
             print("Musisz się najpierw zalogować")
             usrId = input("Podaj swoje imię: ")
         print("Poniżej ulubione koty użytkownika", usrId, "w formacie ID: URL")
-        favouriteIds_dict = view_favourite_ids(view_favourites, usrId) 
-        pprint(favouriteIds_dict)
-        if favouriteIds_dict != {}:
+        favoriteIds_dict = view_favorite_ids(view_favorites, usrId)
+        pprint(favoriteIds_dict)
+        if favoriteIds_dict != {}:
             print()
-            openInBrowser = input("Chcesz otworzyć koty w przeglądarce? T/N: ").upper()
+            openInBrowser = input(
+                "Chcesz otworzyć koty w przeglądarce? T/N: ").upper()
             if openInBrowser == "T":
-                for favIds in favouriteIds_dict:
-                    webbrowser.open_new_tab(favouriteIds_dict[favIds])
+                for favIds in favoriteIds_dict:
+                    webbrowser.open_new_tab(favoriteIds_dict[favIds])
                 print()
                 continue
             else:
@@ -133,24 +144,25 @@ while True:
             print()
             continue
 
-    #usuwamy ulubione
+    # usuwamy ulubione
     if menuChoice == Menu.usun:
-        #sprawdzamy czy użytkownik jest zalogowany
+        # sprawdzamy czy użytkownik jest zalogowany
         if usrId == "":
             print("Musisz się najpierw zalogować")
             usrId = input("Podaj swoje imię: ")
-        deleteFavourites = input("Czy na pewno usuwać ulubione? T/N: ").upper()
-        if deleteFavourites == "T":
-            deleteAll = input("Czy chcesz usunąć wszystkie ulubione? T/N: ").upper()
+        deletefavorites = input("Czy na pewno usuwać ulubione? T/N: ").upper()
+        if deletefavorites == "T":
+            deleteAll = input(
+                "Czy chcesz usunąć wszystkie ulubione? T/N: ").upper()
             if deleteAll == "T":
-                for favIds in view_favourite_ids(view_favourites, usrId):
-                    delete_favourite(usrId, str(favIds))
+                for favIds in view_favorite_ids(view_favorites, usrId):
+                    delete_favorite(usrId, str(favIds))
                 print("Usunięto wszystkie ulubione")
                 print()
                 continue
             else:
                 favId = input("Podaj ID do usunięcia: ")
-                delete_favourite(usrId, favId)
+                delete_favorite(usrId, favId)
                 print("Usunięto kota z ulubionych")
                 print()
                 continue
